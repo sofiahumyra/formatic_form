935d66f8a9ed3793ca0c84c86f874d6710dcf62d
hudakamal <kamalhuda4@gmail.com>
2023-05-02 12:41:37 +0800

    done for multiple authentication

--- app/Http/Middleware/AdminRedirectIfAuthenticated.php
+++ app/Http/Middleware/AdminRedirectIfAuthenticated.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-class RedirectIfAuthenticated
+class AdminRedirectIfAuthenticated
 {
     /**
      * Handle an incoming request.
@@ -17,16 +17,8 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, string ...$guards): Response
     {
-        // $guards = empty($guards) ? [null] : $guards;
-
-        // foreach ($guards as $guard) {
-        //     if (Auth::guard($guard)->check()) {
-        //         return redirect(RouteServiceProvider::HOME);
-        //     }
-        // }
-
-         if (Auth::guard('web')->check()) {
-                return redirect()->route('home');
+            if (Auth::guard('admin')->check()) {
+                return redirect()->route('admin.dashboard');
             } 
 
         return $next($request);
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/user/logout', [App\Http\Controllers\Auth\LoginController::class, 'userLogout'])->name('user.logout');
+
+//Admin
+Route::group(['prefix' => 'admin'], function(){
+    Route::group(['middleware' => 'admin.guest'], function(){
+        Route::view('/login','admin.login')->name('admin.login');
+        Route::post('/login',[App\Http\Controllers\AdminController::class, 'authenticate'])->name('admin.auth');
+    });
+    Route::group(['middleware' => 'admin.auth'], function(){
+        Route::get('/dashboard',[App\Http\Controllers\DashboardController::class, 'dashboard'])->name('admin.dashboard');
+        Route::get('/logout',[App\Http\Controllers\AdminController::class, 'logout'])->name('admin.logout');
+    });
+});
